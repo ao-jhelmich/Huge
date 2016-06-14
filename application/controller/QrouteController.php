529f2d054118b5d1f7ccdaec610485862f413a94
Jasper Helmich <99034085@mydavinci.nl>
2016-06-14 13:28:14 +0200

    Begin mijn project
    Start my project

---
+++ application/controller/QrouteController.php
@@ -0,0 +1,9 @@
+<?php 
+
+class QrouteController extends Controller
+{
+	public function index()
+	{
+		$this->View->render('Qroute/index'); 
+	}
+}
---
+++ application/view/index/index.php
@@ -0,0 +1,33 @@
+<div class="container">
+    <div class="box">
+
+        <!-- echo out the system feedback (error and success messages) -->
+        <?php $this->renderFeedbackMessages(); ?>
+		<table>
+		<thead>
+			<tr>
+				<th>Naam</th>
+     			<th>Maandag</th>
+     			<th>Dinsdag</th>
+     			<th>Woensdag</th>
+     			<th>Donderdag</th>
+     			<th>Vrijdag</th>
+     			<th>Zaterdag</th>
+     			<th>Zondag</th>
+			</tr>
+		</thead>
+		<tbody>
+			<tr>
+				<td>data</td>
+				<td>data</td>
+				<td>data</td>
+				<td>data</td>
+				<td>data</td>
+				<td>data</td>
+				<td>data</td>
+				<td>data</td>
+			</tr>
+		</tbody>
+	</table>
+ </div>
+</div>
